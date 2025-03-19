32ca6812f551bca2199ff96cc998d83e84ba7b9a
Sabina Barila <sabinellbarila@gmail.com>
2025-03-19 23:37:36 +0200

    Am adaugat libraria syncfusion si am generat un circuit de test.
    (cherry picked from commit 45e8cf2dcb5d419744ecc1a059487064d581e95c)

--- Apps/VerilogCircuitAnalyzerUI/src/app/app-routing.module.ts
+++ Apps/VerilogCircuitAnalyzerUI/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CircuitAnalyzerComponent} from './circuit-analyzer/circuit-analyzer.component';
 import {HomeComponent} from './home/home.component';
+import {CircuitDisplayComponent} from './circuit-display/circuit-display.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'circuit-analyzer', component: CircuitAnalyzerComponent },
+  { path: 'circuit-display', component: CircuitDisplayComponent },
 ];
 
 @NgModule({
--- Apps/VerilogCircuitAnalyzerUI/src/main.ts
+++ Apps/VerilogCircuitAnalyzerUI/src/main.ts
@@ -1,5 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { registerLicense } from '@syncfusion/ej2-base';
+import { environment } from './environment/environment';
+
+
+registerLicense(environment.syncfusionkey);
+
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
